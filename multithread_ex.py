e2cbb3535c9d83feb7d2b44baf3d28ece563167a
Glenn Hazelwood <ghazelwood@ltsnet.net>
2020-03-02 17:51:19 -0500

    Switched to multiprocess instead of mult-thread

--- multithread_ex.py
+++ multithread_ex.py
@@ -1,35 +1,45 @@
 #!/usr/bin/env python3
 
-# Script for multithread_ex
-import threading 
-  
-def print_cube(num): 
-    """ 
-    function to print cube of given num 
-    """
-    print("Cube: {}".format(num * num * num)) 
-  
-def print_square(num): 
-    """ 
-    function to print square of given num 
-    """
-    print("Square: {}".format(num * num)) 
+import os
+import math
+from random import random
+from multiprocessing import Process, Queue
+
+def generateData(num_items, data_queue):
+    pname = "generateData"
+    timeout_secs = 2
+    blocking = True
+    for i in range(num_items):
+        t_data = random()
+        print("{}: Generated data {}: {}".format(pname, i, t_data))
+        data_queue.put(t_data, blocking, timeout_secs)
+        print("{}: Data {} put into queue".format(pname, i))
+
+
+def processData(num_items, data_queue):
+    pname = "processData"
+    for i in range(num_items):
+        timeout_secs = 2
+        blocking = True
+        print("{}: Trying to get Data {} from queue".format(pname, i))
+        t_data = data_queue.get(blocking, timeout_secs)
+        
+        print("{}: Got data {}: {}".format(pname, i, t_data))
+        t_data_out = math.sqrt(t_data)
+        print("{}: Result {} added to data_out".format(pname, i))
+    
 
 
 if __name__ == '__main__':
-    # creating thread 
-    t1 = threading.Thread(target=print_square, args=(10,)) 
-    t2 = threading.Thread(target=print_cube, args=(10,)) 
-  
-    # starting thread 1 
-    t1.start() 
-    # starting thread 2 
-    t2.start() 
-  
-    # wait until thread 1 is completely executed 
-    t1.join() 
-    # wait until thread 2 is completely executed 
-    t2.join() 
-  
-    # both threads completely executed 
-    print("Done!")
+    num_items = 5
+    data_queue = Queue()
+    gen_proc = Process(target = generateData, args = (num_items,data_queue,))
+    proc_proc = Process(target = processData, args = (num_items,data_queue,))
+    
+    gen_proc.start()
+    proc_proc.start()
+    
+    gen_proc.join()
+    proc_proc.join()
+    
+    print("Done with both procs. Exiting")
